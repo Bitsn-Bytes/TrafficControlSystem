3ddbff4099b357d0346a11ec41e1dab767762c00
Anusha Potnuru <anusha.sparkx@gmail.com>
2019-02-10 00:19:21 +0530

    modified

--- traffic.py
+++ traffic.py
@@ -1,13 +1,17 @@
 import tensorflow as tf 
 import numpy as np 
 
+# A=[NSG, EWG, NSLG, EWLG]
+
 # global constants
-grid_rows= ;
-grid_cols = ;
+grid_rows= 10
+grid_cols = 10
 action_size=4
+
 # creating our environment
-states = np.zeros((4,grid_rows,grid_cols))
-actions = np.eye(action_size)[np.arange(action_size)]
+states = np.zeros((4, grid_rows, grid_cols))
+actions = np.eye(action_size)
+# [np.arange(action_size)]
 
 # hyperparameters
 
@@ -21,6 +25,7 @@ learning_rate=0.0001
 explore_start= 1.0
 explore_end = 0.01
 decay_rate = 5e-5
+
 # assumed
 gamma = 0.9 
 training = False
@@ -51,7 +56,7 @@ class DQNetwork:
 
 		with tf.variable_scope(name):
 			
-			self.inputs = tf.placeholder(tf.float32,[None,*state_size],name="inputs")
+			self.inputs = tf.placeholder(tf.float32,[None, *state_size] ,name="inputs")
 			self.actions = tf.placeholder(tf.float32, [None, action_size], name="actions")
 			self.target_Q = tf.placeholder(tf.float32,[None],name="target")
 			"""
@@ -62,16 +67,14 @@ class DQNetwork:
 			self.conv = tf.layers.conv2d(inputs = self.inputs,
                                          filters = 32,
                	                         kernel_size = [3,3],
-                   	                     strides = [2,2],
-                       	                 padding = "VALID",
+                       	                 padding = "SAME",
                            	             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
                                	         name = "conv1")
 
 			self.conv1_1 = tf.layers.conv2d(inputs = self.conv,
    	        	                             filters = 32,
                 	                         kernel_size = [3,3],
-                    	                     strides = [2,2],
-                        	                 padding = "VALID",
+                        	                 padding = "SAME",
                             	             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
                                 	         name = "conv1")
             
@@ -85,8 +88,7 @@ class DQNetwork:
 			self.conv1_2 = tf.layers.conv2d(inputs = self.conv1_1_out,
             	                             filters = 32,
                 	                         kernel_size = [3,3],
-                    	                     strides = [2,2],
-                        	                 padding = "VALID",
+                        	                 padding = "SAME",
                             	             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
                                 	         name = "conv1")
             
@@ -107,8 +109,7 @@ class DQNetwork:
 			self.conv2_1 = tf.layers.conv2d(inputs = self.relu1,
            	                             filters = 32,
     	       	                         kernel_size = [3,3],
-                   	                     strides = [2,2],
-                       	                 padding = "VALID",
+                       	                 padding = "SAME",
                            	             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
                                	         name = "conv1")
             
@@ -122,8 +123,7 @@ class DQNetwork:
 			self.conv2_2 = tf.layers.conv2d(inputs = self.conv2_1_out,
             	                             filters = 32,
                 	                         kernel_size = [3,3],
-                    	                     strides = [2,2],
-                        	                 padding = "VALID",
+                        	                 padding = "SAME",
                             	             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
                                 	         name = "conv1")
             
@@ -144,7 +144,7 @@ class DQNetwork:
             self.flatten = tf.layers.flatten(self.relu2)
 
            	self.fc = tf.layers.dense(inputs = self.relu2,
-                               		  units = 512,
+                               		  units = 128, # CHECK
                                		  activation = tf.nn.relu,
                                    	  kernel_initializer=tf.contrib.layers.xavier_initializer(),
                               	      name="fc1") 
@@ -196,34 +196,16 @@ for j in range(agent_size):
         # to_do
         reward = simulation.take_action(action)
     
-        # Look if the episode is finished
-        # to_do
-        done = simulation.is_finished()
+        # Get the next state
+        next_state = simulation.get_state()        
+        # Add experience to memory
+        memory_temp.add_experience((state, action, reward, next_state))
     
-        # If we're dead
-        if done:
-            # We finished the episode
-            next_state = np.zeros(state.shape)
-        
-            # Add experience to memory
-            memory_temp.add_experience((state, action, reward, next_state, done))
-        
-            # Start a new episode
-            # to_do
-            simulation.new_episode()
-        
-            # First we need a state
-            state = simulation.get_state()
-        
-        else:
-            # Get the next state
-            next_state = simulation.get_state()        
-            # Add experience to memory
-            memory_temp.add_experience((state, action, reward, next_state, done))
-        
-            # Our state is now the next_state
-            state = next_state
+        # Our state is now the next_state
+        state = next_state
+
         memory.append(memory_temp)
+
 
 # training the agents
 def predict_action(explore_start, explore_stop, decay_rate, decay_step, state, actions):
@@ -245,114 +227,91 @@ def predict_action(explore_start, explore_stop, decay_rate, decay_step, state, a
     return action, explore_probability
 
 
+with tf.Session() as sess:
+    # Initialize the variables
+    sess.run(tf.global_variables_initializer())
+    decay_step = 0
+
+    # to do
+    simulation.init() 
+    saver = tf.train.Saver()
+
+	for episode in range(agent_network_update): # 1500
+
+		for j in range(agent_size):
+			saver.restore(sess,"./models/model_"+i+".ckpt")
+	 	   	# Set step to 0
+	    	step = 0
+
+	   		 # Initialize the rewards of the episode
+	    	episode_rewards = []
+
+		    # Make a new episode and observe the first state
+		    simulation.new_episode()
+		    state = simulation.get_state()
+
+		    while step < max_steps: #450
+		        step += 1
+
+		        # Increase decay_step
+		        decay_step +=1
+
+		        action = np.zeros((4,4))
+
+		        for i in range(4):
+		            saver.restore(sess,"./models/model_"+i+".ckpt")
+		        # Predict the action to take and take it
+		            action[i], explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step, state, actions)
+
+		        saver.restore(sess, "./models/model_"+j+".ckpt")
+		        # Do the action
+		        reward = simulation.take_action(action) # integrate all into one
+
+		        # Add the reward to total reward
+		        episode_rewards.append(reward)
+
+	            # Get the next state
+	            next_state = simulation.get_state()           
+
+	            # Add experience to memory
+	            memory[i].add_experience((state, action, reward, next_state))
+	    
+	            # st+1 is now our current state
+	            state = next_state
+
+		        if(step % target_network_update == 0):
+		            ### LEARNING PART 
+		            # Obtain random mini-batch from memory
+		            batch = memory[i].sample(batch_size)
+		            states_mb = np.array([each[0] for each in batch], ndmin=3) 
+		            actions_mb = np.array([each[1] for each in batch])
+		            rewards_mb = np.array([each[2] for each in batch]) 
+		            next_states_mb = np.array([each[3] for each in batch], ndmin=3)
+
+		            target_Qs_batch = []
+
+		            # Get Q values for next_state 
+		            Qs_next_state = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: next_states_mb})
+
+		            # Set Q_target = r if the episode ends at s+1, otherwise set Q_target = r + gamma*maxQ(s', a')
+		            for i in range(0, len(batch)):
+		                target = rewards_mb[i] + gamma * np.max(Qs_next_state[i])
+		                target_Qs_batch.append(target)	            
+
+		            targets_mb = np.array([each for each in target_Qs_batch])
+
+		            loss, _ = sess.run([DQNetwork.loss, DQNetwork.optimizer],
+		                            feed_dict={DQNetwork.inputs_: states_mb,
+		                                       DQNetwork.target_Q: targets_mb,
+		                                       DQNetwork.actions_: actions_mb})
+
+		    total_reward = np.sum(episode_rewards)
+		    print('Episode: {}'.format(episode),    
+		              'Total reward: {}'.format(total_reward),
+		              'Training loss: {:.4f}'.format(loss),
+		              'Explore P: {:.4f}'.format(explore_probability))
+		    
+
+		save_path = saver.save(sess, "./models/model_"+j+".ckpt")
 
 
-if training == True:
-
-	
-    	with tf.Session() as sess:
-            # Initialize the variables
-        	sess.run(tf.global_variables_initializer())
-            decay_step = 0
-
-            # to_do
-            simulation.init()
-            saver = tf.train.Saver()
-            
-            for k in range(total_episodes):
-                for i in range(agent_size):
-                    if(i!=0):
-                        saver.restore(sess,"./models/model_"+i+".ckpt")
-                    for episode in range(agent_network_update):
-                        # Set step to 0
-                        step = 0
-            
-                        # Initialize the rewards of the episode
-                        episode_rewards = []
-            
-                        # Make a new episode and observe the first state
-                        simulation.new_episode()
-                        state = simulation.get_state()
-          
-                        while step < max_steps:
-                            step += 1
-                    
-                            # Increase decay_step
-                            decay_step +=1
-                
-                            # Predict the action to take and take it
-                            action, explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step, state, actions)
-
-                            # Do the action
-                            reward = simulation.take_action(action)
-
-                            # Look if the episode is finished
-                            done = simulation.is_finished()
-                
-                            # Add the reward to total reward
-                            episode_rewards.append(reward)
-
-                            # If the game is finished
-                            if done:
-                                # the episode ends so no next state
-                                next_state = np.zeros((grid_rows,grid_cols,4), dtype=np.int)
-                                # Set step = max_steps to end the episode
-                                step = max_steps
-                                # Get the total reward of the episode
-                                total_reward = np.sum(episode_rewards)
-
-                                print('Episode: {}'.format(episode),    
-                                  'Total reward: {}'.format(total_reward),
-                                  'Training loss: {:.4f}'.format(loss),
-                                  'Explore P: {:.4f}'.format(explore_probability))
-
-                                memory[i].add_experience((state, action, reward, next_state, done))
-
-                            else:
-                                # Get the next state
-                                next_state = simulation.get_state()              
-
-                                # Add experience to memory
-                                memory[i].add_experience((state, action, reward, next_state, done))
-                        
-                                # st+1 is now our current state
-                                state = next_state
-
-                            if(episode%target_network_update==0):
-                                ### LEARNING PART            
-                                # Obtain random mini-batch from memory
-                                batch = memory[i].sample(batch_size)
-                                states_mb = np.array([each[0] for each in batch], ndmin=3)
-                                actions_mb = np.array([each[1] for each in batch])
-                                rewards_mb = np.array([each[2] for each in batch]) 
-                                next_states_mb = np.array([each[3] for each in batch], ndmin=3)
-                                dones_mb = np.array([each[4] for each in batch])
-
-                                target_Qs_batch = []
-
-                                # Get Q values for next_state 
-                                Qs_next_state = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: next_states_mb})
-                    
-                                # Set Q_target = r if the episode ends at s+1, otherwise set Q_target = r + gamma*maxQ(s', a')
-                                for i in range(0, len(batch)):
-                                    terminal = dones_mb[i]
-
-                                    # If we are in a terminal state, only equals reward
-                                    if terminal:
-                                        target_Qs_batch.append(rewards_mb[i])
-                                
-                                    else:
-                                        target = rewards_mb[i] + gamma * np.max(Qs_next_state[i])
-                                        target_Qs_batch.append(target)
-                                
-
-                                targets_mb = np.array([each for each in target_Qs_batch])
-
-                                loss, _ = sess.run([DQNetwork.loss, DQNetwork.optimizer],
-                                                feed_dict={DQNetwork.inputs_: states_mb,
-                                                           DQNetwork.target_Q: targets_mb,
-                                                           DQNetwork.actions_: actions_mb})
-
-                        save_path = saver.save(sess, "./models/model_"+i+".ckpt")                    
-                    
-                    k += agent_network_update 
