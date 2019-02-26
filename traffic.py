bb33ff7b72784aca76f143368a021573be87b813
Anusha Potnuru <anusha.sparkx@gmail.com>
2019-02-27 00:10:46 +0530

    minor changes

--- traffic.py
+++ traffic.py
@@ -6,7 +6,7 @@ import numpy as np
 # global constants
 grid_rows= 10
 grid_cols = 10
-action_size=4
+action_size= 4
 
 # creating our environment
 states = np.zeros((4, grid_rows, grid_cols))
@@ -33,15 +33,17 @@ pretrain_length = batch_size
 target_network_update = 150
 agent_network_update = 1500
 
+agent_size = 2
+
 class Simulation:
-    def __init__():
+		def __init__():
 
-    def get_state(self):
+		def get_state(self): # move to next state and get matrices
 
-    def take_action(self, action):
-        self.reward = 
+		def take_action(self, action): # change light and get reward
+				self.reward = 
 
-    # def is_finished(self):
+		# def is_finished(self):
 
 
 # initialize simualtion 
@@ -65,41 +67,41 @@ class DQNetwork:
 			
 			"""
 			self.conv = tf.layers.conv2d(inputs = self.inputs,
-                                         filters = 32,
-               	                         kernel_size = [3,3],
-                       	                 padding = "SAME",
-                           	             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                               	         name = "conv1")
+																				 filters = 32,
+																				 kernel_size = [3,3],
+																				 padding = "SAME",
+																				 kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+																				 name = "conv1")
 
 			self.conv1_1 = tf.layers.conv2d(inputs = self.conv,
-   	        	                             filters = 32,
-                	                         kernel_size = [3,3],
-                        	                 padding = "SAME",
-                            	             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                                	         name = "conv1")
-            
-            self.conv1_1_batchnorm = tf.layers.batch_normalization(self.conv1_1,
-                        	                          				training = True,
-                	                                   				epsilon = 1e-5,
-                    	                                			name = 'batch_norm1')
-            
-           	self.conv1_1_out = tf.nn.relu(self.conv1_1_batchnorm, name="conv1_out")
+																					 filters = 32,
+																					 kernel_size = [3,3],
+																					 padding = "SAME",
+																					 kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+																					 name = "conv1")
+						
+						self.conv1_1_batchnorm = tf.layers.batch_normalization(self.conv1_1,
+																														training = True,
+																														epsilon = 1e-5,
+																														name = 'batch_norm1')
+						
+						self.conv1_1_out = tf.nn.relu(self.conv1_1_batchnorm, name="conv1_out")
 
 			self.conv1_2 = tf.layers.conv2d(inputs = self.conv1_1_out,
-            	                             filters = 32,
-                	                         kernel_size = [3,3],
-                        	                 padding = "SAME",
-                            	             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                                	         name = "conv1")
-            
-           	self.conv1_2_batchnorm = tf.layers.batch_normalization(self.conv1_2,
-                        	                          				training = True,
-                	                                   				epsilon = 1e-5,
-                    	                                			name = 'batch_norm1')
-            	
-           	self.input2 = tf.math.add(self.conv,self.conv1_2_batchnorm,name='add1')
+																					 filters = 32,
+																					 kernel_size = [3,3],
+																					 padding = "SAME",
+																					 kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+																					 name = "conv1")
+						
+						self.conv1_2_batchnorm = tf.layers.batch_normalization(self.conv1_2,
+																														training = True,
+																														epsilon = 1e-5,
+																														name = 'batch_norm1')
+							
+						self.input2 = tf.math.add(self.conv,self.conv1_2_batchnorm,name='add1')
 
-           	"""
+						"""
 			Second convnet: 
 			CNN BN Relu CNN BN
 			
@@ -107,210 +109,210 @@ class DQNetwork:
 			self.relu1 = tf.nn.relu(self.input2, name="relu1") 
 
 			self.conv2_1 = tf.layers.conv2d(inputs = self.relu1,
-           	                             filters = 32,
-    	       	                         kernel_size = [3,3],
-                       	                 padding = "SAME",
-                           	             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                               	         name = "conv1")
-            
-           	self.conv2_1_batchnorm = tf.layers.batch_normalization(self.conv2_1,
-                        	                          				training = True,
-                	                                   				epsilon = 1e-5,
-                    	                                			name = 'batch_norm1')
-            
-            self.conv2_1_out = tf.nn.relu(self.conv2_1_batchnorm, name="conv1_out")
+																				 filters = 32,
+																			 kernel_size = [3,3],
+																				 padding = "SAME",
+																				 kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+																				 name = "conv1")
+						
+						self.conv2_1_batchnorm = tf.layers.batch_normalization(self.conv2_1,
+																														training = True,
+																														epsilon = 1e-5,
+																														name = 'batch_norm1')
+						
+						self.conv2_1_out = tf.nn.relu(self.conv2_1_batchnorm, name="conv1_out")
 		
 			self.conv2_2 = tf.layers.conv2d(inputs = self.conv2_1_out,
-            	                             filters = 32,
-                	                         kernel_size = [3,3],
-                        	                 padding = "SAME",
-                            	             kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
-                                	         name = "conv1")
-            
-            self.conv2_2_batchnorm = tf.layers.batch_normalization(self.conv2_2,
-                        	                          				training = True,
-                	                                   				epsilon = 1e-5,
-                    	                                			name = 'batch_norm1')
-            	
-            self.input3 = tf.math.add(self.relu1,self.conv2_2_batchnorm,name='add2')
+																					 filters = 32,
+																					 kernel_size = [3,3],
+																					 padding = "SAME",
+																					 kernel_initializer=tf.contrib.layers.xavier_initializer_conv2d(),
+																					 name = "conv1")
+						
+						self.conv2_2_batchnorm = tf.layers.batch_normalization(self.conv2_2,
+																														training = True,
+																														epsilon = 1e-5,
+																														name = 'batch_norm1')
+							
+						self.input3 = tf.math.add(self.relu1,self.conv2_2_batchnorm,name='add2')
 
-           	"""
-           	Final output
+						"""
+						Final output
 
-           	Relu FC 
-           	"""
-            self.relu2 = tf.nn.relu(self.input3, name="relu2")
+						Relu FC 
+						"""
+						self.relu2 = tf.nn.relu(self.input3, name="relu2")
 
-            self.flatten = tf.layers.flatten(self.relu2)
+						self.flatten = tf.layers.flatten(self.relu2)
 
-           	self.fc = tf.layers.dense(inputs = self.relu2,
-                               		  units = 128, # CHECK
-                               		  activation = tf.nn.relu,
-                                   	  kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                              	      name="fc1") 
+						self.fc = tf.layers.dense(inputs = self.relu2,
+																		units = 128, # CHECK
+																		activation = tf.nn.relu,
+																			kernel_initializer=tf.contrib.layers.xavier_initializer(),
+																			name="fc1") 
 
-           	self.output = tf.layers.dense(inputs = self.fc, 
-                                     	  kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                                       	  units = action_size, 
-                                      	  activation=None)
+						self.output = tf.layers.dense(inputs = self.fc, 
+																				kernel_initializer=tf.contrib.layers.xavier_initializer(),
+																					units = action_size, 
+																					activation=None)
 
-           	self.Q = tf.reduce_sum(tf.multiply(self.output, self.actions), axis=1)
+						self.Q = tf.reduce_sum(tf.multiply(self.output, self.actions), axis=1)
 
-           	self.loss = tf.reduce_mean(tf.square(self.target_Q - self.Q))
-            
-            self.optimizer = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
+						self.loss = tf.reduce_mean(tf.square(self.target_Q - self.Q))
+						
+						self.optimizer = tf.train.RMSPropOptimizer(self.learning_rate).minimize(self.loss)
 
  
 class ReplayMemory():
 	def __init__(self, max_size):
-        self.buffer = deque(maxlen = max_size)
-    
-    def add_experience(self, experience):
-        self.buffer.append(experience)
-    
-    def sample(self, batch_size):
-        buffer_size = len(self.buffer)
-        index = np.random.choice(np.arange(buffer_size),
-                                size = batch_size,
-                                replace = False)
-        
-        return [self.buffer[i] for i in index]
+				self.buffer = deque(maxlen = max_size)
+		
+		def add_experience(self, experience):
+				self.buffer.append(experience)
+		
+		def sample(self, batch_size):
+				buffer_size = len(self.buffer)
+				index = np.random.choice(np.arange(buffer_size),
+																size = batch_size,
+																replace = False)
+				
+				return [self.buffer[i] for i in index]
 
 # instantiate memory
 memory = []
 
 for j in range(agent_size):
-    memory_temp = ReplayMemory(max_size=memory_size)
+		memory_temp = ReplayMemory(max_size=memory_size)
 
-    for i in range(pretrain_length):
-        # If it's the first step
-        if i == 0:
-            # First we need a state shape(state) = grid_rows*grid_cols*4
-            # to_do
-            state = simulation.get_state()
-    
-        # Random action
-        action = random.choice(actions)
-    
-        # Get the rewards
-        # to_do
-        reward = simulation.take_action(action)
-    
-        # Get the next state
-        next_state = simulation.get_state()        
-        # Add experience to memory
-        memory_temp.add_experience((state, action, reward, next_state))
-    
-        # Our state is now the next_state
-        state = next_state
+		for i in range(pretrain_length):
+				# If it's the first step
+				if i == 0:
+						# First we need a state shape(state) = grid_rows*grid_cols*4
+						# to_do
+						state = simulation.get_state()
+		
+				# Random action
+				action = random.choice(actions)
+		
+				# Get the rewards
+				# to_do
+				reward = simulation.take_action(action)
+		
+				# Get the next state
+				next_state = simulation.get_state()        
+				# Add experience to memory
+				memory_temp.add_experience((state, action, reward, next_state))
+		
+				# Our state is now the next_state
+				state = next_state
 
-        memory.append(memory_temp)
+				memory.append(memory_temp)
 
 
 # training the agents
 def predict_action(explore_start, explore_stop, decay_rate, decay_step, state, actions):
-    
-    tradeoff = np.random.rand()
+		
+		tradeoff = np.random.rand()
 
-    # Here we'll use an improved version of our epsilon greedy strategy used in Q-learning notebook
-    explore_probability = explore_stop + (explore_start - explore_stop) * np.exp(-decay_rate * decay_step)
-    
-    if (explore_probability > tradeoff):
-        # Make a random action (exploration)
-        action = random.choice(actions)
-        
-    else:
-        Qs = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: state.reshape((1, *state.shape))})
-        choice = np.argmax(Qs)
-        action = actions[int(choice)]
-                
-    return action, explore_probability
+		# Here we'll use an improved version of our epsilon greedy strategy used in Q-learning notebook
+		explore_probability = explore_stop + (explore_start - explore_stop) * np.exp(-decay_rate * decay_step)
+		
+		if (explore_probability > tradeoff):
+				# Make a random action (exploration)
+				action = random.choice(actions)
+				
+		else:
+				Qs = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: state.reshape((1, *state.shape))})
+				choice = np.argmax(Qs)
+				action = actions[int(choice)]
+								
+		return action, explore_probability
 
 
 with tf.Session() as sess:
-    # Initialize the variables
-    sess.run(tf.global_variables_initializer())
-    decay_step = 0
+		# Initialize the variables
+		sess.run(tf.global_variables_initializer())
+		decay_step = 0
 
-    # to do
-    simulation.init() 
-    saver = tf.train.Saver()
+		# to do
+		simulation.init() 
+		saver = tf.train.Saver()
 
 	for episode in range(agent_network_update): # 1500
 
 		for j in range(agent_size):
 			saver.restore(sess,"./models/model_"+i+".ckpt")
-	 	   	# Set step to 0
-	    	step = 0
+				# Set step to 0
+				step = 0
 
-	   		 # Initialize the rewards of the episode
-	    	episode_rewards = []
+				 # Initialize the rewards of the episode
+				episode_rewards = []
 
-		    # Make a new episode and observe the first state
-		    # simulation.new_episode()
-		    state = simulation.get_state()
+				# Make a new episode and observe the first state
+				# simulation.new_episode()
+				state = simulation.get_state()
 
-		    while step < max_steps: #450
-		        step += 1
+				while step < max_steps: #450
+						step += 1
 
-		        # Increase decay_step
-		        decay_step +=1
+						# Increase decay_step
+						decay_step +=1
 
-		        action = np.zeros((4,4))
+						action = np.zeros((agent_size, action_size))
 
-		        for i in range(4):
-		            saver.restore(sess,"./models/model_"+i+".ckpt")
-		        # Predict the action to take and take it
-		            action[i], explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step, state, actions)
+						for i in range(agent_size):
+								saver.restore(sess,"./models/model_"+i+".ckpt")
+						# Predict the action to take and take it
+								action[i], explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step, state, actions)
 
-		        saver.restore(sess, "./models/model_"+j+".ckpt")
-		        # Do the action
-		        reward = simulation.take_action(action) # integrate all into one
+						saver.restore(sess, "./models/model_"+j+".ckpt")
+						# Do the action
+						reward = simulation.take_action(action) # integrate all into one
 
-		        # Add the reward to total reward
-		        episode_rewards.append(reward)
+						# Add the reward to total reward
+						episode_rewards.append(reward)
 
-	            # Get the next state
-	            next_state = simulation.get_state()           
+							# Get the next state
+							next_state = simulation.get_state()
 
-	            # Add experience to memory
-	            memory[i].add_experience((state, action, reward, next_state))
-	    
-	            # st+1 is now our current state
-	            state = next_state
+							# Add experience to memory
+							memory[i].add_experience((state, action, reward, next_state))
+			
+							# st+1 is now our current state
+							state = next_state
 
-		        if(step % target_network_update == 0):
-		            ### LEARNING PART 
-		            # Obtain random mini-batch from memory
-		            batch = memory[i].sample(batch_size)
-		            states_mb = np.array([each[0] for each in batch], ndmin=3) 
-		            actions_mb = np.array([each[1] for each in batch])
-		            rewards_mb = np.array([each[2] for each in batch]) 
-		            next_states_mb = np.array([each[3] for each in batch], ndmin=3)
+						if(step % target_network_update == 0):
+								### LEARNING PART 
+								# Obtain random mini-batch from memory
+								batch = memory[i].sample(batch_size)
+								states_mb = np.array([each[0] for each in batch], ndmin=3) 
+								actions_mb = np.array([each[1] for each in batch])
+								rewards_mb = np.array([each[2] for each in batch]) 
+								next_states_mb = np.array([each[3] for each in batch], ndmin=3)
 
-		            target_Qs_batch = []
+								target_Qs_batch = []
 
-		            # Get Q values for next_state 
-		            Qs_next_state = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: next_states_mb})
+								# Get Q values for next_state 
+								Qs_next_state = sess.run(DQNetwork.output, feed_dict = {DQNetwork.inputs_: next_states_mb})
 
-		            # Set Q_target = r if the episode ends at s+1, otherwise set Q_target = r + gamma*maxQ(s', a')
-		            for i in range(0, len(batch)):
-		                target = rewards_mb[i] + gamma * np.max(Qs_next_state[i])
-		                target_Qs_batch.append(target)	            
+								# Set Q_target = r if the episode ends at s+1, otherwise set Q_target = r + gamma*maxQ(s', a')
+								for i in range(0, len(batch)):
+										target = rewards_mb[i] + gamma * np.max(Qs_next_state[i])
+										target_Qs_batch.append(target)	            
 
-		            targets_mb = np.array([each for each in target_Qs_batch])
+								targets_mb = np.array([each for each in target_Qs_batch])
 
-		            loss, _ = sess.run([DQNetwork.loss, DQNetwork.optimizer],
-		                            feed_dict={DQNetwork.inputs_: states_mb,
-		                                       DQNetwork.target_Q: targets_mb,
-		                                       DQNetwork.actions_: actions_mb})
+								loss, _ = sess.run([DQNetwork.loss, DQNetwork.optimizer],
+																feed_dict={DQNetwork.inputs_: states_mb,
+																					 DQNetwork.target_Q: targets_mb,
+																					 DQNetwork.actions_: actions_mb})
 
-		    total_reward = np.sum(episode_rewards)
-		    print('Episode: {}'.format(episode),    
-		              'Total reward: {}'.format(total_reward),
-		              'Training loss: {:.4f}'.format(loss),
-		              'Explore P: {:.4f}'.format(explore_probability))
-		    
+				total_reward = np.sum(episode_rewards)
+				print('Episode: {}'.format(episode),    
+									'Total reward: {}'.format(total_reward),
+									'Training loss: {:.4f}'.format(loss),
+									'Explore P: {:.4f}'.format(explore_probability))
+				
 
 		save_path = saver.save(sess, "./models/model_"+j+".ckpt")
 
